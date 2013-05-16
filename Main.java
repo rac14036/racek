6f46aa10cf4fd170a4bc2cf878b3fa2041d3958f
rac14036 <rac14036@spengergasse.at>
2013-05-16 16:36:28 +0200

    Moved Thomas to own class

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ public class Main{
 
 	public static void main(string[] args){
 		String name="Haberfellner";
-		String name="Thomas";
+		String name=Thomas.getThomas();
 		String name="Daniel";
 		String name="Scheucher";
 		System.out.println("Hallo Welt, ich heiße "+name+"!");
---
+++ Thomas.java
@@ -0,0 +1,5 @@
+public class Thomas{
+	public static String getThomas(){
+		return "Thomas";
+	}
+}
